cc3cbe0d72c805692188023360aaa17d974a01b5
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-06-25 13:01:41 +0530

    my first cgi program

---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Content-type:text/html")
+print("")
+print("<html><body>")
+print("<h1>My code working very well</h1>")
+for i in range(5):
+    print("<h2>hello world!"+str(i)+"</h2>")
+print("</body></html>")
